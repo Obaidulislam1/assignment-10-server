d9a3ca54fe0034aafccd96fb6ef88a48f84eb22d
obaidul islam1 <mursalinislam537215@gmail.com>
2022-10-26 11:43:51 +0600

    server course data upload

--- index.js
+++ index.js
@@ -5,18 +5,21 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors())
-
+const courses = require('./data/courseData.json');
 const students = require('./data/data.json');
 
 app.get('/', (req,res) =>{
     res.send('assignment 10 server is on')
+})
+app.get('/courses', (req,res) =>{
+    res.send(courses)
 })
 app.get('/students', (req,res) =>{
     res.send(students);
 })
 app.get('/students/:id',(req,res) =>{
   const id =  parseInt(req.params.id);
-  const student =students.find(st => st.id === id);
+  const student =students.map(st => st.id === id);
   res.send(student);
 })
 app.listen(port, () =>{
